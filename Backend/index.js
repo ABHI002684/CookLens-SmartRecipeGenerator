f0b4e18ea80405db0d9deb12e6b7c9f8d993a812
ABHI002684 <abhishek002684@gmail.com>
2025-11-21 20:03:54 +0530

    Setup backend and database

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+
+
+const app = express();
+app.use(express.json());
+
+
+connectDB();
+
+
+
+
+
+// basic error middleware
+app.use((err, req, res, next) => {
+console.error(err.stack);
+res.status(err.status || 500).json({ error: err.message || 'Server Error' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
